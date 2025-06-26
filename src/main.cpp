1ae0cb77915c12a9ddc432eb632f973e1c304c84
claujunior <claudivan.junior.114@ufrn.edu.br>
2025-06-25 21:02:41 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -29,162 +29,130 @@
  *
  * @remark On 2021-09-19 changed ...
  */
-#include <cstdlib> // EXIT_SUCCESS
+#include <cstdlib>       // EXIT_SUCCESS
+#include <iostream>
+#include <string>
+#include <vector>
+#include <filesystem>    // std::filesystem::exists
+#include <thread>        // std::this_thread::sleep_for
+#include <chrono>        // std::chrono::milliseconds
+
 #include "libs/animation.h"
 #include "libs/hate.h"
-#include <filesystem> //exists
-#include <chrono> //duration
-#include <thread> //sleep_for
-//macro para a string indefinida
-// #include "bcr_am.h"
+
 #define UNDEFINED_STRING "-#@"
 
-struct Argument_Options{
-  bool send_help=false;
-  bool is_all_invalid=false;
-  //"-b 60"
-  int bar_argument=-1;
-  //"-f 24"
-  int fps_argument=-1;
-  std::string ini_file = UNDEFINED_STRING;
-  std::string text_file = UNDEFINED_STRING;
+struct ArgumentOptions {
+    bool send_help = false;
+    bool is_all_invalid = false;
+    int bar_argument = -1;
+    int fps_argument = -1;
+    std::string ini_file = UNDEFINED_STRING;
+    std::string text_file = UNDEFINED_STRING;
 };
-///@brief Handles the command line arguments
-///@param argc the args
-///@param argv the argv 
-///@param options the running options
 
-void handle_argv(int argc, char* argv[], Argument_Options &options){
-  std::vector<std::string> strings;
-  if(argc == 1){
-    options.is_all_invalid=true;
-    return;
-  }
-  for(int i=1; i<argc; ++i){
-    strings.push_back(argv[i]);
-  } 
-  int str_sz=strings.size();
-  for(int i=0; i<str_sz; ++i){
-    if(strings[i][0] == '-'){
-      if(strings[i]=="-b"){
-        if(++i<str_sz){
-          int b=std::stoi(strings[i]);
-          if(b > 5){
-            options.bar_argument=b;
-          }
-        }
-      }
-      else if(strings[i]=="-f"){
-        if(++i<str_sz){
-          int fps=std::stoi(strings[i]);
-          if(fps > 0){
-            options.fps_argument=fps;
-          }
-        }
-      }
-      else if(strings[i]=="-b"){
-        if(++i<str_sz){
-          int f=std::stoi(strings[i]);
-          if(f > 0){
-            options.bar_argument=f;
-          }
-        }
-      }
-      else if(strings[i] == "-h"){
-        options.send_help=true;
-        return;
-      }
-    }
-    else{
-      //é um candidato a ini
-      size_t pos = strings[i].rfind('.');
-      if(pos!= std::string::npos){
-        std::string temp = strings[i].substr(pos);
-        if(temp == ".ini"){
-          if(options.ini_file == UNDEFINED_STRING){
-            options.ini_file=strings[i];
-          }
-        }
-        else{
-          if(std::filesystem::exists(strings[i])){
-            options.text_file=strings[i];
-          }
-        }
-      }
-      else{
-        if(std::filesystem::exists(strings[i])){
-            options.text_file=strings[i];
-          }
-      }
-    }
-  }
-  return;
-}
-///prints the help text
-void send_help()
-{
-  std::string help = R"(
-  
+void print_help() {
+    const std::string help = R"(
     Welcome to the Bar Chart Race program.
-    You need to provide a formated data file to run the program
-    run with the following command
-    ./bcr {mandatory: <DATA_FILE>} {optional: <ini file with settings>} {optional: <command line arguments}
+
+    You need to provide a formatted data file to run the program.
+    Run with the following command:
+      ./bcr <DATA_FILE> [ini_file] [command line arguments]
+
     Command line arguments:
-
-    -b <interger> sets the max amount of horizontal bars to be displayed
-    -f <interger> sets the framerate of the program
-    
+      -b <integer>  Sets the max number of horizontal bars to be displayed (must be > 5)
+      -f <integer>  Sets the framerate of the program (must be > 0)
+      -h            Displays this help message
     )";
-  std::cout << help;
-}
-///@brief Handles the given options and edits the AnimationController class
-///@param options the options struct
-///@param animations The AnimationController class
-
-void handle_options(const Argument_Options &options, AnimationController &animation){
-  if(options.is_all_invalid){
-    //help
-    send_help();
-    exit(1);
-  }
-  if(options.send_help){
-    send_help();
-    exit(1);
-  }
-  if(options.text_file != UNDEFINED_STRING){
-    animation.setfilename(options.text_file); 
-  }
-  else{
-    std::cerr << "Data file not found. Use the argument -h to learn more";
-  }
-  if(options.ini_file != UNDEFINED_STRING){
-    animation.readini(options.ini_file); 
-  }
-  if(options.bar_argument!=-1){
-    animation.set_bars(options.bar_argument);
-  }
-  if(options.fps_argument != -1){
-    animation.set_fps(options.fps_argument);
-  }
+    std::cout << help << '\n';
 }
 
-int main(int argc, char **argv) {
-  Argument_Options options;
+void handle_arguments(int argc, char* argv[], ArgumentOptions& options) {
+    if (argc == 1) {
+        options.is_all_invalid = true;
+        return;
+    }
 
-  handle_argv(argc, argv, options);
+    std::vector<std::string> args(argv + 1, argv + argc);
 
-  AnimationController animation;
+    for (size_t i = 0; i < args.size(); ++i) {
+        const std::string& arg = args[i];
 
-  handle_options(options, animation);
-  
+        if (arg[0] == '-') {
+            if (arg == "-b" && i + 1 < args.size()) {
+                try {
+                    int bars = std::stoi(args[++i]);
+                    if (bars > 5) {
+                        options.bar_argument = bars;
+                    }
+                } catch (...) { /* silently ignore */ }
+            }
+            else if (arg == "-f" && i + 1 < args.size()) {
+                try {
+                    int fps = std::stoi(args[++i]);
+                    if (fps > 0) {
+                        options.fps_argument = fps;
+                    }
+                } catch (...) { /* silently ignore */ }
+            }
+            else if (arg == "-h") {
+                options.send_help = true;
+                return;
+            }
+            else {
+                options.is_all_invalid = true;
+            }
+        } 
+        else {
+            const auto dot_pos = arg.rfind('.');
+            if (dot_pos != std::string::npos && arg.substr(dot_pos) == ".ini") {
+                if (options.ini_file == UNDEFINED_STRING) {
+                    options.ini_file = arg;
+                }
+            } else if (std::filesystem::exists(arg)) {
+                options.text_file = arg;
+            }
+        }
+    }
+}
 
-  // The Game Loop architecture or design pattern.
-  while (not animation.is_over()) {
-    animation.process_events();
-    animation.update();
-    //std::this_thread::sleep_for(std::chrono::milliseconds(50));
-    animation.render();
-  }
+void apply_options(const ArgumentOptions& options, AnimationController& animation) {
+    if (options.is_all_invalid || options.send_help) {
+        print_help();
+        exit(1);
+    }
 
-  return EXIT_SUCCESS;
+    if (options.text_file != UNDEFINED_STRING) {
+        animation.setfilename(options.text_file);
+    } else {
+        std::cerr << "Data file not found. Use the argument -h to learn more.\n";
+        exit(1);
+    }
+
+    if (options.ini_file != UNDEFINED_STRING) {
+        animation.readini(options.ini_file);
+    }
+    if (options.bar_argument != -1) {
+        animation.set_bars(options.bar_argument);
+    }
+    if (options.fps_argument != -1) {
+        animation.set_fps(options.fps_argument);
+    }
+}
+
+int main(int argc, char** argv) {
+    ArgumentOptions options;
+    handle_arguments(argc, argv, options);
+
+    AnimationController animation;
+    apply_options(options, animation);
+
+    // Game loop
+    while (!animation.is_over()) {
+        animation.process_events();
+        animation.update();
+        animation.render();
+    }
+
+    return EXIT_SUCCESS;
 }
